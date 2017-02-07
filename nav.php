b2838cd3d67dd0ff19eae5236f9be0a9b55531b9
Giuseppe Spallone <Giuseppe Spallone>
2017-02-07 11:44:18 +0100

    Sessione 1.00001

--- nav.php
+++ nav.php
@@ -1,38 +1,57 @@
 <?php
+include 'check/SessionController.php';
 $print_class = 'class="active"';
 ?>
 
 <nav class="navbar navbar-default navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a href="#" class="navbar-brand">Popcorn</a> </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li <?= ($selector == 0)?$print_class:'' ?> ><a href="index.php">Home</a></li>
-                    <li <?= ($selector == 1)?$print_class:'' ?>><a href="programmazione.php">Programmazione</a></li>
-                    <li <?= ($selector == 2)?$print_class:'' ?>><a href="prezzi.php">Prezzi e Sale</a></li>
-                    <li <?= ($selector == 3)?$print_class:'' ?>><a href="contatti.php">Contatti</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li id="fat-menu" class="dropdown"> <a href="#" class="dropdown-toggle" id="drop3" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Il mio account <span class="caret"></span> </a>
-                        <ul class="dropdown-menu" aria-labelledby="drop3">
-                            <?php
-                            if ($is_logged) {
-                                echo '<li> <a href="check/disconnessione.php">Disconnetti</a></li>';
-                            }else {
-                                echo '<li> <a href="accesso_registrazione.php">Accedi</a></li>';
-                            }
-                            ?>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">Popcorn</a></div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li <?= ($selector == 0) ? $print_class : '' ?> ><a href="index.php">Home</a></li>
+                <li <?= ($selector == 1) ? $print_class : '' ?>><a href="programmazione.php">Programmazione</a></li>
+                <li <?= ($selector == 2) ? $print_class : '' ?>><a href="prezzi.php">Prezzi e Sale</a></li>
+                <li <?= ($selector == 3) ? $print_class : '' ?>><a href="contatti.php">Contatti</a></li>
+            </ul>
 
+            <?php
+            if ($is_logged) {
+                ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li id="fat-menu" class="dropdown">
+                        <a href="#" class="dropdown-toggle" id="drop3" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <?php echo $username; ?>
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="drop3">
+                            <li><a href="check/disconnessione.php">Disconnetti</a></li>
                         </ul>
                     </li>
                 </ul>
-            </div>
+                <?php
+            } else {
+                ?>
+                <ul class="nav navbar-nav navbar-right">
+                    <li id="fat-menu" class="dropdown"><a href="#" class="dropdown-toggle" id="drop3"
+                                                          data-toggle="dropdown"
+                                                          role="button" aria-haspopup="true" aria-expanded="false"> Il
+                            mio
+                            account <span class="caret"></span> </a>
+                        <ul class="dropdown-menu" aria-labelledby="drop3">
+                            <li><a href="accesso_registrazione.php">Accedi</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <?php
+            }
+            ?>
         </div>
-    </nav>
+    </div>
+</nav>
 <br> <br> <br>
