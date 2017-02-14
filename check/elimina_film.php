938917106bce9546d8d6f9e72bc458700856bc6d
Giuseppe <giuseppe.spallone95@gmail.com>
2017-02-14 13:51:58 +0100

    funzionante anche la cancellazione delle righe orari e sale del film

--- check/elimina_film.php
+++ check/elimina_film.php
@@ -6,14 +6,27 @@ $db_instance = new DbManager();
 $titolo = $_REQUEST['elimina'];
 $titolo = str_replace('_', ' ', $titolo);
 
-//cancellazione prog_film
+//recupero id film
 $id_film = $db_instance->select(array('id'), 'film', "titolo = '$titolo'")->fetch_array();
 $id_film = $id_film['id'];
 
+//recupero id orari e sale
+$id = $db_instance->select(array('id_orari', 'id_sale'), 'prog_film', "id_film = '$id_film'")->fetch_array();
+$id_orari = $id['id_orari'];
+$id_sale = $id['id_sale'];
+
+//cancellazione programmazione orari e sale del film
 $result = $db_instance->queryString("DELETE FROM prog_film WHERE id_film = '$id_film'");
+
+//cancellazione orari del film
+$result = $db_instance->queryString("DELETE FROM orari WHERE id_orari = '$id_orari'");
+
+//cancellazione sale del film
+$result = $db_instance->queryString("DELETE FROM sale_film WHERE id = '$id_sale'");
 
 //cancellazione film
 $result = $db_instance->queryString("DELETE FROM film WHERE titolo = '$titolo'");
+
 
 
 
