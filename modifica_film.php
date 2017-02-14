5d50d90da8d2ed1bd5dab9a33f4a2096a2db31d6
Giuseppe <giuseppe.spallone95@gmail.com>
2017-02-14 16:11:11 +0100

    Aggiunti button con immagini a modifica film

--- modifica_film.php
+++ modifica_film.php
@@ -21,7 +21,6 @@ include 'nav.php';
 
 <div class="container">
     <div class="row">
-
         <div class="col-sm-6 col-md-4">
             <div class="thumbnail">
                 <a href="form_film.php" role="button">
@@ -29,10 +28,16 @@ include 'nav.php';
 
                 <div class="caption">
                     <h3> Inserisci un nuovo film </h3>
+                    <button class="btn btn-danger"><span class="glyphicon glyphicon-trash"></button>
+                    <button class="btn btn-warning"><span class="glyphicon glyphicon-pencil"></span>
+                    </button>
+                    <button class="btn btn-primary"><span class="glyphicon glyphicon-film"></span>
+                    </button>
                 </div>
 
             </div>
         </div>
+
 
         <?php while ($film = $resultFilm->fetch_array(MYSQLI_ASSOC)) {
             $replace_film = str_replace(' ', '_', $film['titolo']);
@@ -46,72 +51,14 @@ include 'nav.php';
 
                     <div class="caption">
                         <h3> <?php echo $film['titolo'] ?> </h3>
-                        <?php echo '<form id="elimina" action="check/elimina_film.php?elimina=' . $replace_film . ' " method="post" role="form">'; ?>
 
-                        <div class="form-group">
-
-                             <button  type="submit" class="btn btn-danger" tabindex="">Elimina
-                            </button>
-
-                            <!-- <input name="elimina-film" type="button" tabindex="" id="elimina-film"
-                                   class="btn btn-danger" value="Elimina">
-                            </input> -->
-                        </div>
-
-                        <div class="modal fade" id="eliminaModal" tabindex="-1" role="dialog" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="form-group">
-                                        <div class="modal-header">
-                                            <h2>Elimina film </h2>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="modal-body">Sei sicuro di voler eliminare il film?
-                                        </div>
-                                    </div>
-
-                                    <div class="modal-footer">
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-sm-6 col-sm-offset-3">
-                                                    <input type="submit" name="elimina-submit" id="elimina-submit"
-                                                           tabindex="4"
-                                                           class="form-control btn btn-register" value="Si">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="row">
-                                                <div class="col-sm-6 col-sm-offset-3">
-                                                    <input data-dismiss="modal" tabindex="5"
-                                                           class="form-control btn btn-register" value="No">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        </form>
-
-                        <?php echo '<form id="modifica-film" action="form_modifica_film.php?modifica=' . $replace_film . '" method="post" role="form">'; ?>
-                        <input type="submit" name="modifica-submit" id="modifica-submit" tabindex=""
-                               class="btn btn-warning" value="Modifica">
-                        </form>
-
-                        <?php echo '<form id="modifica-film" action="form_modifica_orari_sale.php?modifica=' . $replace_film . '" method="post" role="form">'; ?>
-                        <input type="submit" name="modifica-submit" id="modifica-submit" tabindex=""
-                               class="btn btn-primary" value="Orari e sale">
-                        </form>
-
+                        <?php echo '<button class="btn btn-danger"> <a href="check/elimina_film.php?elimina=' . $replace_film . '"> <span class="glyphicon glyphicon-trash"> </a> </button>' ?>
+                        <?php echo '<button class="btn btn-warning"> <a href="form_modifica_film.php?modifica=' . $replace_film . '"> <span class="glyphicon glyphicon-pencil"></span> </a> </button>' ?>
+                        <?php echo '<button class="btn btn-primary"> <a href="form_modifica_orari_sale.php?modifica=' . $replace_film . '"> <span class="glyphicon glyphicon-film"></span> </a> </button>' ?>
                     </div>
-
                 </div>
             </div>
         <?php } ?>
-
     </div>
 </div>
 
