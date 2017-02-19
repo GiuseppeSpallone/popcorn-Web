e0c568c96a18c10aabb6fc92d90d6f20c5b3fcc0
LauZol <laura.zollo00@gmail.com>
2017-02-19 09:28:46 +0100

    //TODO debug index: non funziona next e previous

---
+++ js/carosello.js
@@ -0,0 +1,20 @@
+/**
+ * Created by laura on 2/19/2017.
+ */
+$(document).ready(function () {
+
+
+    for (index=0; index < titoli.length; index++){
+
+        if (index == 0) {
+
+            $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="' + index + '" class="active"></li>');
+            $(".carousel-inner").append('<div class="item active"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div>');
+        } else {
+
+            $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="' + index + '"></li>')
+            $(".carousel-inner").append('<div class ="item"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div>');
+        }
+    }
+
+});
