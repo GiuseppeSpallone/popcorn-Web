f7bb9411d260acd451d6644fb003cc5e5d4a524c
LauZol <laura.zollo00@gmail.com>
2017-02-19 09:28:22 +0100

    //TODO debug index: non funziona next e previous

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ include 'check/stampa_film.php';
     <link rel="icon" href="/logo/Pop.png" type="image/png"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
+    <script src="js/carosello.js"></script>
+
 
 
     <script>
@@ -49,10 +50,20 @@ include 'nav.php';
     <br>
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
 
+        <script>  var titoli = [];</script>
+
+        <?php
+        while ($film = $resultFilm->fetch_array(MYSQLI_ASSOC)) { ?>
+            <script> titoli.push("<?php echo $film["titolo"] ?>") ;</script>
+            <?php
+        }
+        ?>
+
+        <script>  console.log(titoli);        </script>
 
         <ol class="carousel-indicators">
-            <?php
-            $i = 0;
+           <!-- --><?php
+/*            $i = 0;
             while ($film = $resultFilm->fetch_array(MYSQLI_ASSOC)) {
                 if ($i == 0) {
                     echo '<li data-target="#myCarousel" data-slide-to="' . $i . '" class="active"></li>';
@@ -63,58 +74,51 @@ include 'nav.php';
                 $i++;
             }
 
-            ?>
+            */?>
 
         </ol>
 
 
-
-
-
-        <!--
-          --><?php
-        $i = 0;
-        while ($films = $resultFilm->fetch_array(MYSQLI_ASSOC)) {
-            $replace_film = str_replace(' ', '_', $films['titolo']);
-            if ($i == 0) {
-                echo '<div class="item active"><img src="image/' . $replace_film . '.jpg" width="460" height="345">
-                        <div class="carousel-caption">
-                        </div>
-                    </div>';
-
-            } else {
-                echo '
-                    <div class="item"><img src="image/' . $replace_film . '.jpg" width="460" height="345">
-                        <div class="carousel-caption">
-                        </div>
+        <div class="carousel-inner" role="listbox">
+   <!--         --><?php
+/*            $i = 0;
+            while ($film = $resultFilm->fetch_array(MYSQLI_ASSOC)) {
+                if ($i == 0) {
+                    echo '<div class="item active"><img src="image/' . $replace_film . '.jpg" width="460" height="345">
+                    <div class="carousel-caption">
                     </div>
-                    ';
+                </div>';
+
+                } else {
+                    echo ' <div class="item"><img src="image/' . $replace_film . '.jpg" width="460" height="345">
+                    <div class="carousel-caption">
+                    </div>
+                </div>';
+                }
+                $i++;
             }
-            $i++;
-        }
 
-        ?>
+            */?>
 
 
+        </div>
+        <!--Left and right controls-->
+        <a class="left carousel-control" href="#myCarousel" role="button" data - slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria - hidden="true"></span>
+            <span class="sr-only"> Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data - slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria - hidden="true"></span>
+            <span class="sr-only"> Next</span>
+        </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
 </div>
 
 
-<div id="user" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel"
-     aria-hidden="true">
+<div id="user" class="modal fade" tabindex="-1" role="dialog" aria - labelledby="mySmallModalLabel"
+     aria - hidden="true">
     <div class="modal-dialog modal-sm">
-        <div class="alert alert-success" role="alert">Benvenuto su Popcorn <?php echo $username ?></div>
+        <div class="alert alert-success" role="alert"> Benvenuto su Popcorn <?php echo $username ?></div>
     </div>
 </div>
 
@@ -125,4 +129,7 @@ include 'nav.php';
     </div>
 </div>
 </body>
+
+
+
 </html>
