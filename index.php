6aecd5cf10bf75fbb5d52095bfdeaa1d57d7dbe9
LauZol <laura.zollo00@gmail.com>
2017-02-19 09:36:08 +0100

    //TODO debug index: non funziona next e previous

--- index.php
+++ index.php
@@ -103,12 +103,12 @@ include 'nav.php';
 
         </div>
         <!--Left and right controls-->
-        <a class="left carousel-control" href="#myCarousel" role="button" data - slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria - hidden="true"></span>
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only"> Previous</span>
         </a>
-        <a class="right carousel-control" href="#myCarousel" role="button" data - slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria - hidden="true"></span>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only"> Next</span>
         </a>
     </div>
--- js/carosello.js
+++ js/carosello.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
         if (index == 0) {
 
             $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="' + index + '" class="active"></li>');
-            $(".carousel-inner").append('<div class="item active"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div>');
+            $(".carousel-inner").append('<div class="item active"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div></div>');
         } else {
 
             $(".carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="' + index + '"></li>')
-            $(".carousel-inner").append('<div class ="item"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div>');
+            $(".carousel-inner").append('<div class ="item"><img src="image/' +titoli[index]+ '.jpg" width="460" height="345"><div class="carousel-caption"></div></div>');
         }
     }
 
